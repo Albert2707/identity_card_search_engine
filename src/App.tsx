3797a3e5af33ad87df2e13de2508689cf8f3a209
Albert2707 <202010652@itla.edu.do>
2023-01-18 17:14:31 -0400

    firm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Req from "./Components/Form/Req";
 import UserCard from "./Components/User/UserCard";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Footer from "./Components/Footer/Footer";
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App  mt-14 container mx-auto">
-        <Req />
+      <div className="min-h-screen flex flex-col">
+        <div className=" mt-14 container mx-auto flex-1">
+          <Req />
+        </div>
+        <Footer />
+
       </div>
     </QueryClientProvider>
   );
